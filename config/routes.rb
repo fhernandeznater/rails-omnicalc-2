e7290c0e4641bea3f4d88461a76a34c4e28f7e2b
Federico Hernandez Nater <166563763+fhernandeznater@users.noreply.github.com>
2025-03-28 04:30:05 +0000

    Fixed some names

--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,13 @@ Rails.application.routes.draw do
   get("/add", { :controller => "addition", :action => "show_addition_form" })
   get("/wizard_addition", { :controller => "addition", :action => "add_these" })
 
-  get("/subtract", { :controller => "subtract", :action => "show_sub_form" })
-  get("/wizard_subtract", { :controller => "subtraction", :action => "subtract" })
+  get("/subtract", { :controller => "subtraction", :action => "show_sub_form" })
+  get("/wizard_subtract", { :controller => "subtraction", :action => "subtract_these" })
 
   get("/multiply", { :controller => "multiplication", :action => "multiplication" })
-  get("/wizard_multiply", { :controller => "multiplication", :action => "multiply" })
+  get("/wizard_multiply", { :controller => "multiplication", :action => "multiply_these" })
 
   get("/divide", { :controller => "division", :action => "show_division_form" })
-  get("/wizard_divide", { :controller => "div", :action => "divide_these" })
+  get("/wizard_divide", { :controller => "division", :action => "divide_these" })
 
 end
