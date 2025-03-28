0d2472510d7d2b5dac096f163c092575e9d12a50
Federico Hernandez Nater <166563763+fhernandeznater@users.noreply.github.com>
2025-03-28 04:36:18 +0000

    Some name matching and copy

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,10 @@ Rails.application.routes.draw do
   get("/add", { :controller => "addition", :action => "show_addition_form" })
   get("/wizard_addition", { :controller => "addition", :action => "add_these" })
 
-  get("/subtract", { :controller => "subtraction", :action => "show_sub_form" })
+  get("/subtract", { :controller => "subtraction", :action => "show_subtraction_form" })
   get("/wizard_subtract", { :controller => "subtraction", :action => "subtract_these" })
 
-  get("/multiply", { :controller => "multiplication", :action => "multiplication" })
+  get("/multiply", { :controller => "multiplication", :action => "show_multiplication_form" })
   get("/wizard_multiply", { :controller => "multiplication", :action => "multiply_these" })
 
   get("/divide", { :controller => "division", :action => "show_division_form" })
